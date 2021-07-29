8ce863dbdaccb6d0f8e9a9129124eca58ca8d157
Andi <1vandidroid1@gmail.com>
2021-07-29 17:43:15 +0200

    Initial State

---
+++ src/main/java/com/vandidroid/digitalnomaddestinations/DigitalNomadDestinationsApplication.java
@@ -0,0 +1,13 @@
+package com.vandidroid.digitalnomaddestinations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalNomadDestinationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalNomadDestinationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vandidroid/digitalnomaddestinations/DigitalNomadDestinationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandidroid.digitalnomaddestinations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalNomadDestinationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
